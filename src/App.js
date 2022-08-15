54a51d3e5752c74dce2380e167ac0ba820076c21
Daniel <danieldeandradelopes@gmail.com>
2022-08-15 15:36:47 -0300

    create problem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [numberType, SetNumberType] = useState(0);
+
+  function handleSetNumberType() {
+    if (count % 2 !== 0) {
+      return SetNumberType(`Número impar`);
+    }
+
+    SetNumberType(`Número par`);
+  }
+
+  useEffect(() => {
+    handleSetNumberType();
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <h1>
+          {count}
+        </h1>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+      <h1>{numberType}</h1>
     </div>
   );
 }
